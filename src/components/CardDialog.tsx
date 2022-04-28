5c939f199d996372ceb9b8935ea808c5222fac1d
jgunzelman <jgunzelman88@gmail.com>
2022-04-27 20:57:09 -0400

    ✨ basic sorting

--- src/components/CardDialog.tsx
+++ src/components/CardDialog.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import {Card} from '../model/Card'
+import { Card, Price } from '../model/Card'
+import  Paper  from '@mui/material/Paper';
 
 interface Props {
     card: Card
 }
 
-export class CardDialog extends React.Component<Props> {
+class State {
+    public prices = new Array<Price>()
+}
 
+export class CardDialog extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = new State()
+    }
+
+    render() {
+        return (
+            <div>
+                <Paper>
+                    
+                </Paper>
+            </div>
+        )
+    }
 }
