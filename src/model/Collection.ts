b8838ea614311901c6b37840daba60136c40c42e
jgunzelman <jgunzelman88@gmail.com>
2022-04-29 18:25:22 -0400

    👔 change model

--- src/model/Collection.ts
+++ src/model/Collection.ts
@@ -1,13 +1,13 @@
-import { v4 as uuidv4 } from 'uuid';
 import { Card } from './Card';
 
 export class Collection {
-    public id: string = uuidv4()
+    public id: number 
     public name: string
     public icon?: string 
     public cards = new Array<Card>()
 
-    constructor(name: string){
+    constructor(id: number, name: string){
+        this.id = id
         this.name = name
     }
 }
