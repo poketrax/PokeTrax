03ba61e6b1571b67eaa93c58da4fdc2644979563
jgunzelman <jgunzelman88@gmail.com>
2022-04-30 10:42:08 -0400

    💄 spinners everywhere

--- src/components/CardCase.tsx
+++ src/components/CardCase.tsx
@@ -5,6 +5,7 @@ import { getRarity, getTCGPprice } from '../controls/CardDB';
 import { CgPokemon } from "react-icons/cg"
 import CircularProgress from '@mui/material/CircularProgress';
 import Paper from '@mui/material/Paper';
+import { Tooltip } from '@mui/material';
 
 interface Props {
     card: Card
@@ -12,6 +13,7 @@ interface Props {
 
 class State {
     public prices = new Array<Price>()
+    public imgLoaded = false
 }
 
 export class CardCase extends React.Component<Props, State> {
@@ -20,44 +22,69 @@ export class CardCase extends React.Component<Props, State> {
         this.state = new State()
         getTCGPprice(props.card).then(
             (value) => {
-                this.setState({...this.state, prices: value})
+                this.setState({ ...this.state, prices: value })
             }
         )
     }
     render() {
         return (
             <div className='flex justify-center'>
-                <Paper elevation={3} className='rounded-md w-64 h-fit hover:shadow-2xl hover:hover:bg-red-600 hover:text-white'>
+                <Paper elevation={3} className='rounded-md w-64 h-fit hover:shadow-2xl hover:bg-blue-500 hover:text-white'>
                     <div className='h-8 flex items-center'>
                         {this.getEnergy(this.props.card?.energyType ?? "")}
                         <span className='pl-2 truncate' >{this.props.card?.name}</span>
                     </div>
-                    <div className='flex justify-items-center'>
-                        <img className='w-64' src={baseURL + "/cardImg/" + this.props.card?.cardId} onError={(ev) => { if (ev.target instanceof HTMLImageElement) ev.target.src = './assests/pokemon-back.png' }} />
+                    <div style={{ position: 'relative', }}>
+                        {this.imgSpinner()}
+                        <img className='w-64'
+                            style={{ visibility: this.state.imgLoaded ? 'visible' : 'hidden' }}
+                            src={baseURL + "/cardImg/" + this.props.card?.cardId}
+                            onLoad={() => this.setState({ ...this.state, imgLoaded: true })}
+                            onError={(ev) => { if (ev.target instanceof HTMLImageElement) ev.target.src = './assests/pokemon-back.png' }}
+                        />
                     </div>
                     <div className='h-8 flex justify-items-center items-center'>
                         <div className='flex justify-items-center items-center h-8 w-8 ml-2'>
-                            <img className='h-6' src={baseURL + "/expSymbol/" + this.props.card?.expName} />
+                            <Tooltip title={this.props.card.expName}>
+                                <img className='h-6' src={baseURL + "/expSymbol/" + this.props.card?.expName} />
+                            </Tooltip>
                         </div>
                         <div className='grow'></div>
-                        <a className='text-blue-600' href={'https://tcgplayer.com/product/' + this.props.card?.idTCGP}>{this.getPrice()}</a>
+                        <a href={'https://tcgplayer.com/product/' + this.props.card?.idTCGP}>{this.getPrice()}</a>
                         <div className='grow'></div>
                         <div>{this.props.card?.expCardNumber}</div>
                         <div className='grow'></div>
-                        <div className='flex justify-items-center items-center h-8 w-8'>
-                            {getRarity(this.props.card?.rarity ?? "")}
-                        </div>
+                        <Tooltip title={this.props.card.rarity}>
+                            <div className='flex justify-items-center items-center h-8 w-8'>
+                                {getRarity(this.props.card?.rarity ?? "")}
+                            </div>
+                        </Tooltip>
                     </div>
                 </Paper>
             </div>
         )
     }
 
-    componentWillReceiveProps(props: Props){
-        if(props.card.cardId !== this.props.card.cardId){
+    imgSpinner() {
+        if (this.state.imgLoaded === false) {
+            console.log('spin to win')
+            return (
+                <div className="h-full" style={{ position: 'absolute' }}>
+                    <div className='flex items-center justify-center w-64 h-full'>
+                        <CircularProgress className="flex" size={100} ></CircularProgress>
+                    </div>
+                </div>
+
+            )
+        }
+    }
+
+    componentWillReceiveProps(props: Props) {
+        if (props.card.cardId !== this.props.card.cardId) {
+            this.setState(new State())
             getTCGPprice(props.card).then(
                 (data) => {
-                   this.setState({...this.state, prices: data})
+                    this.setState({ ...this.state, prices: data })
                 }
             )
         }
