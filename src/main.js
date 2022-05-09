c5935372507740477b45739ca851198e72a2bb41
jgunzelman <jgunzelman88@gmail.com>
2022-05-08 20:45:00 -0400

    🐛 one slipped though

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ let mainWindow;
 
 function createWindow () {
   const startUrl = process.env.ELECTRON_START_URL ||url.format({
-        pathname: path.join(mw.pwd(), './index.html'),
+        pathname: path.join(DB.pwd(), './index.html'),
         protocol: 'file:',
         slashes: true,
       });
