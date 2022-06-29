fca105a8076697c1f2c7b62d1bce0fbf1d767b1c
jgunzelman <jgunzelman88@gmail.com>
2022-06-29 17:17:34 -0400

    remove wait

--- test/rest-tests.js
+++ test/rest-tests.js
@@ -123,8 +123,7 @@ describe(
         })
 
         it('Test Get Cards', async () => {
-            await new Promise(resolve => setTimeout(resolve, 5000));
-            console.log("Done waiting, Calling get cards")
+            //await new Promise(resolve => setTimeout(resolve, 5000));
             let resCol1 = await axios.get("http://localhost:3030/collections/collection1/cards/0")
             let resCol2 = await axios.get("http://localhost:3030/collections/collection2/cards/0")
             assert.equal(resCol1.data.total, 3, `Number of cards in Collection 1 not right ${JSON.stringify(resCol1.data)}`)
