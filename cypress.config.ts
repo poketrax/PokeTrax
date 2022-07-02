014dcab68ae46e312c6c5294175db0c6412097e1
jgunzelman <jgunzelman88@gmail.com>
2022-07-02 15:19:14 -0400

    ✅ migration to cypress 10

---
+++ cypress.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "bjcr3a",
+  defaultCommandTimeout: 180000,
+
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+});
