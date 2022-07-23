af36faacb0172a4a082327ef60f80ce5f172d2ed
jgunzelman <jgunzelman88@gmail.com>
2022-07-23 18:49:59 -0400

    ➕ updated deps to latest

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "./",
-    plugins: [react()]
+    plugins: [react()],
+    server: {
+        port: 3000
+    }
 })
