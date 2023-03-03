5ce0a27af609f15ea9ff692ee049e9e3bbd6d080
jgunzelman <jgunzelman88@gmail.com>
2023-03-03 08:30:42 -0500

    🚨

--- src-web/src/components/Collection/ProdcutCollection.svelte
+++ src-web/src/components/Collection/ProdcutCollection.svelte
@@ -7,4 +7,7 @@
 
 {#if view === 'Products'}
 	<CollectionToggle />
+	<div class="lg:h-[calc(100vh-12rem)] sm:h-[calc(100vh-17rem)]  w-screen overflow-hidden">
+		<div class="flex lg:h-[calc(100vh-12rem)] sm:h-[calc(100vh-17rem)] w-screen overflow-auto" />
+	</div>
 {/if}
